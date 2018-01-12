e14ada386017d4eb2bd90e3fd29f0fe49d952c14
ccq <1192797580@qq.com>
2018-01-12 17:10:45 +0800

    提交测试

---
+++ src/component/App.vue
@@ -0,0 +1,16 @@
+<template id="app">
+    <div>测试环境</div>
+</template>
+
+<script>
+    export default{
+        name:'app'
+    }
+</script>
+
+<style scoped>
+#app{
+    color: red;
+}
+</style>
+
