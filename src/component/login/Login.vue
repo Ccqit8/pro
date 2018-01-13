b5ad030fafa807d2274f71f849d5ffa2a5b7b0bf
ccq <1192797580@qq.com>
2018-01-13 08:48:54 +0800

    集成 vue-router and axios Ectest

---
+++ src/component/login/Login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>登录页面</div>
+</template>
+<script>
+    var axios=require('axios');
+  
+    export default {
+      
+        created(){
+            this.$http.get(this.$api.login).then(res=>{
+                console.log(res.data);
+            })
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
